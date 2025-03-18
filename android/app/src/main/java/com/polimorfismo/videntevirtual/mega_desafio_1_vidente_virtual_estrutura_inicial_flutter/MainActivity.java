a95823391c6b8f4b2a504117d30b3c33d133a726
Lucas Fagundes <lucasfagundesvaz@gmail.com>
2025-03-18 07:59:26 -0300

    Atualização do projeto inicial

--- android/app/src/main/java/com/polimorfismo/videntevirtual/mega_desafio_1_vidente_virtual_estrutura_inicial_flutter/MainActivity.java
+++ android/app/src/main/java/com/polimorfismo/videntevirtual/mega_desafio_1_vidente_virtual_estrutura_inicial_flutter/MainActivity.java
@@ -1,4 +1,4 @@
-package videntevirtualflutter.suportemaster.vidente_virtual_flutter;
+package com.polimorfismo.videntevirtual.mega_desafio_1_vidente_virtual_estrutura_inicial_flutter;
 
 import io.flutter.embedding.android.FlutterActivity;
 
